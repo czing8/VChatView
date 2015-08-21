206e24f76b884d4f61f3c5ba93296127bdf3c028
Vols <zheng7998@gmail.com>
2015-08-21 16:07:21 +0800

    no message

--- VChatView/VChatView/VChatView.m
+++ VChatView/VChatView/VChatView.m
@@ -8,7 +8,7 @@
 
 #import "VChatView.h"
 
-#define  kArrowHeight  8
+#define  kArrowWidth  8
 #define  kArrowCurvature 5
 
 #define  kInterval  5
@@ -30,7 +30,7 @@
     
     [self.borderColor set];     //设置线条颜色
     
-    CGRect frame = CGRectMake(kInterval, kInterval + 10, self.bounds.size.width - kInterval*2, self.bounds.size.height - kInterval*2 - 10);
+    CGRect frame = CGRectMake(kInterval+10, kInterval + 10, self.bounds.size.width - kInterval*2 - 10, self.bounds.size.height - kInterval*2 - 10);
     
     float xMin = CGRectGetMinX(frame);
     float yMin = CGRectGetMinY(frame);
@@ -39,23 +39,12 @@
     
     //    CGPoint arrowPoint = [self convertPoint:self.showPoint fromView:_handerView];
     
-    CGPoint arrowPoint = CGPointMake(50, 2);
+    CGPoint arrowPoint = CGPointMake(8, 50);
     
     UIBezierPath *popoverPath = [UIBezierPath bezierPath];
     
     [popoverPath moveToPoint:CGPointMake(xMin, yMin+kMaxCurvature*_curvature)];    //左上角
     [popoverPath addQuadCurveToPoint:CGPointMake(xMin+kMaxCurvature*_curvature, yMin) controlPoint:CGPointMake(xMin, yMin)];
-    
-    /********************向上的箭头**********************/
-    [popoverPath addLineToPoint:CGPointMake(arrowPoint.x - kArrowHeight, yMin)];//left side
-    [popoverPath addCurveToPoint:arrowPoint
-                   controlPoint1:CGPointMake(arrowPoint.x - kArrowHeight + kArrowCurvature, yMin)
-                   controlPoint2:arrowPoint];//actual arrow point
-    
-    [popoverPath addCurveToPoint:CGPointMake(arrowPoint.x + kArrowHeight, yMin)
-                   controlPoint1:arrowPoint
-                   controlPoint2:CGPointMake(arrowPoint.x + kArrowHeight - kArrowCurvature, yMin)];//right side
-    /**************************************************/
     
     [popoverPath addLineToPoint:CGPointMake(xMax-kMaxCurvature*_curvature, yMin)];//右上角
     [popoverPath addQuadCurveToPoint:CGPointMake(xMax, yMin+kMaxCurvature*_curvature) controlPoint:CGPointMake(xMax, yMin)];
@@ -65,6 +54,18 @@
     
     [popoverPath addLineToPoint:CGPointMake(xMin+kMaxCurvature*_curvature, yMax)];//左下角
     [popoverPath addQuadCurveToPoint:CGPointMake(xMin, yMax - kMaxCurvature*_curvature) controlPoint:CGPointMake(xMin, yMax)];
+    
+    
+    /********************向左的箭头**********************/
+    [popoverPath addLineToPoint:CGPointMake(xMin, arrowPoint.y + kArrowWidth)];//left side
+    [popoverPath addCurveToPoint:arrowPoint
+                   controlPoint1:CGPointMake(xMin, arrowPoint.y + kArrowWidth - kArrowCurvature)
+                   controlPoint2:arrowPoint];//actual arrow point
+    
+    [popoverPath addCurveToPoint:CGPointMake(xMin, arrowPoint.y - kArrowWidth)
+                   controlPoint1:arrowPoint
+                   controlPoint2:CGPointMake(xMin, arrowPoint.y - kArrowWidth + kArrowCurvature)];
+    /**************************************************/
     
     //填充颜色
     [kRGB(245, 245, 245) setFill];
